9e82309d9f001aac0c7e8b87303211d5e9ffff86
LakshayKaushik2103 <lakshaykaushik01@gmail.com>
2022-02-07 20:03:24 +0530

    working on registration form

--- src/component/register/register.jsx
+++ src/component/register/register.jsx
@@ -1,7 +1,67 @@
+import React, { useState } from "react";
+import { Wrapper, Div } from "./style";
+import { Typography, Button, Avatar } from "@material-ui/core";
+import { TextField } from "@mui/material";
+
 export default function Register() {
-    return (
-      <main style={{ padding: "1rem 0" }}>
-        <h2>Register</h2>
-      </main>
-    );
-  }
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    console.log("email is ", email);
+    console.log("password is ", password);
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
+    <Wrapper>
+      <Div>
+      <Avatar>H</Avatar>
+      </Div>
+      <Div>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+      </Div>
+      <form>
+        <Div>
+          <TextField
+            id="email"
+            label="Email"
+            type="email"
+            variant="outlined"
+            value={email}
+            onChange={handleEmail}
+            sx={{ m: 1, width: '40ch' }}
+          />
+        </Div>
+        <Div>
+          <TextField
+            id="password"
+            label="Password"
+            type="password"
+            variant="outlined"
+            value={password}
+            onChange={handlePassword}
+            sx={{ m: 1, width: '40ch' }}
+          />
+        </Div>
+        <br />
+        <Div>
+          <Button variant="contained" color="primary" fullWidth size="large" onClick={handleSubmit}>
+            Login
+          </Button>
+        </Div>
+      </form>
+    </Wrapper>
+  );
+}
