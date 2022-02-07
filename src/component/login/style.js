ac94e81292f5c13be06791c70527a4d91ce76696
Arunkumar2611 <arunkumar2611@github.com>
2022-02-07 19:07:16 +0530

    complete login form

---
+++ src/component/login/style.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.section`
+  padding: 1em;
+  background: papayawhip;
+  width: auto;
+  height: auto;
+  flex-direction: column;
+    display: inline-flex;
+    align-items: center;
+    margin-block: 80px;
+`;
+
+export const Div= styled.div`
+padding: 0.2em;
+color: gray;
+`;
+
