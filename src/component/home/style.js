a62c83ed87ff6445098f86b88f8bddac38a4181a
Arunkumar2611 <arunkumar2611@github.com>
2022-02-09 10:55:42 +0530

    adding background image

--- src/component/home/style.js
+++ src/component/home/style.js
@@ -1,12 +1,19 @@
 import styled from "styled-components"
-
+import image from '../../img/background.jpg'
 export const StyledItem = styled.div`
 .typography {
   font-weight: 600;
   font-size: 40px;
   color: white;
   width: 100%;
+}
 
+.containerss {
+  background-image: 
+  url("https://www.itchotels.com/content/dam/itchotels/in/umbrella/images/headmast-desktop/welcomhotel-bhubaneswar.jpg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
 }
 .datePicker {
   color: blue;
